a6a02261f87a87a153b23c4367bea82703a214d7
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-03-17 10:16:26 +0100

    Added basic middlewares

--- src/app.js
+++ src/app.js
@@ -3,3 +3,9 @@ const app = express();
 const database = require('./config/database');
 const http = require('http');
 const server = http.createServer(app);
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json({ limit : '100mb' }))
+app.use(express.urlencoded({ limit : '100mb' }));
+
