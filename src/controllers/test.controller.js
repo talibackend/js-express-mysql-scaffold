84e78368df4532752dd050cd29968e25f15e7d89
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-03-18 17:04:12 +0100

    Done with the route

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -1,4 +1,5 @@
 const { helloValidation } = require('../utils/validations/test.validations.js');
+const { helloService } = require('../services/test.service');
 
 module.exports = {
     hello : async (req, res)=>{
@@ -6,6 +7,8 @@ module.exports = {
 
         try{
             let payload = helloValidation(body);
+            let service = await helloService(payload);
+            return res.status(service.status).json({...service});
         }catch(error){
             console.error(error);
             return res.status(error.status).json({...error});
