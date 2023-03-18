8bc2120656b3fda95828c32b3425673c955e0d83
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-03-18 17:06:58 +0100

    Updated validation

--- src/utils/validations/test.validations.js
+++ src/utils/validations/test.validations.js
@@ -5,7 +5,8 @@ module.exports = {
     helloValidation : (body)=>{
         const schema = Joi.object({
             name : Joi.string().required(),
-            email : Joi.string().email().required()
+            email : Joi.string().email().required(),
+            password : Joi.string().min(8)
         });
 
         return validator(schema, body);
